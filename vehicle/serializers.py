2cea5b3f5db8ee2693673be9dac09187290e6bbe
AKIBBB <aaakibmonir@gmail.com>
2025-01-16 21:59:46 +0600

    change

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -1,14 +1,24 @@
 from rest_framework import serializers
-from .import models
+from . import models
 
 class RideRequestSerializer(serializers.ModelSerializer):
     class Meta:
-        model=models.RiderRequest
-        fields='__all__'
+        model = models.RiderRequest
+        fields = '__all__'
         
-    
-    
+        extra_kwargs = {
+            'rent': {'required': False}  
+        }
+
+    def create(self, validated_data):
+        rent = validated_data.pop('rent', None)
+        instance = super().create(validated_data)
+        if rent is not None:
+            instance.rent = rent
+            instance.save()
+        return instance
+
 class RentCalculationSerializer(serializers.Serializer):
     address = serializers.CharField(max_length=255)
     destination = serializers.CharField(max_length=255)
-    totalRent = serializers.FloatField()
+    totalRent = serializers.FloatField(read_only=True)  
